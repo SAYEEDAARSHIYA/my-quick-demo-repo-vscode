596061b93dec2eb1b1f7cb5a7cdf2a722ca15e72
SAYEEDAARSHIYA <arshiyasayeeda00@gmail.com>
2025-07-04 11:32:18 +0530

    Remove Python files

--- hello.py
+++
@@ -1,9 +0,0 @@
-# hello_success.py
-
-def greet(name):
-    return f"Hello, {name}! This build will succeed."
-
-if __name__ == "__main__":
-    user = input("Enter your name: ")
-    print(greet(user))
-    exit(0)  # Success
--- hello_fail.py
+++
@@ -1,9 +0,0 @@
-# hello_fail.py
-
-def greet(name):
-    return f"Hello, {name}! This build will fail."
-
-if __name__ == "__main__":
-    user = input("Enter your name: ")
-    print(greet(user))
-    exit(1)  # Failure
